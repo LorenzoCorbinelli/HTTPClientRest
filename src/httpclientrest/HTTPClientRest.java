ce7e22781f1d77f7fa2e1978caf4a924426e5b4b
LorenzoCorbinelli <lore.corbi01@gmail.com>
2020-02-26 10:46:09 +0100

    Implemented all method

--- src/httpclientrest/HTTPClientRest.java
+++ src/httpclientrest/HTTPClientRest.java
@@ -1,13 +1,73 @@
 package httpclientrest;
 
+import java.util.Scanner;
+
 public class HTTPClientRest 
 {
     public static void main(String[] args) 
     {
         RestClient client = new RestClient();
+        Scanner input = new Scanner(System.in);
         
-      //  client.post();
-        client.get();
+        int id;
+        String nome;
+        String cognome;
+        String email;
+        String numero;
+        String s;
+        do
+        {
+            System.out.println("1. get");
+            System.out.println("2. post");
+            System.out.println("3. delete");
+            System.out.println("4. get(id)");
+            System.out.println("5. put");
+            System.out.println("0. uscita");
+            s = input.nextLine();
+            switch(s)
+            {
+                case "1":
+                    client.get();
+                break;
+                case "2":
+                    System.out.println("Inserisci l'ID:");
+                    id=Integer.parseInt(input.nextLine());
+                    System.out.println("Inserisci il nome:");
+                    nome=input.nextLine();
+                    System.out.println("Inserisci il cognome:");
+                    cognome=input.nextLine();
+                    System.out.println("Inserisci l'email:");
+                    email=input.nextLine();
+                    System.out.println("Inserisci il telefono:");
+                    numero=input.nextLine();
+                    client.post(id,nome,cognome,email,numero);
+                break;
+                case "3":
+                    System.out.println("Inserissci l'ID:");
+                    id=Integer.parseInt(input.nextLine());
+                    client.delete(id);
+                break;
+                case "4":
+                    System.out.println("Inserissci l'ID:");
+                    id=Integer.parseInt(input.nextLine());
+                    client.getId(id);
+                break;
+                case "5":
+                    System.out.println("Inserisci l'ID:");
+                    id=Integer.parseInt(input.nextLine());
+                    System.out.println("Inserisci il nome:");
+                    nome=input.nextLine();
+                    System.out.println("Inserisci il cognome:");
+                    cognome=input.nextLine();
+                    System.out.println("Inserisci l'email:");
+                    email=input.nextLine();
+                    System.out.println("Inserisci il telefono:");
+                    numero=input.nextLine();
+                    client.put(id,nome,cognome,email,numero);
+                break;
+            }
+            System.out.println("");
+        }while(!s.equals("0"));
     }
     
 }
--- src/httpclientrest/RestClient.java
+++ src/httpclientrest/RestClient.java
@@ -20,9 +20,6 @@ public class RestClient
         try
         {
             url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
-            conn = (HttpURLConnection) url.openConnection();
-            br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-            os = conn.getOutputStream();
         }catch(Exception e){}
     }
     
@@ -30,34 +27,14 @@ public class RestClient
     {
         try
         {
-            conn.connect();
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
+            conn = (HttpURLConnection) url.openConnection();
+            
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
-            conn.getResponseCode();
-            output = br.readLine();
-            while(output!=null)
-            {
-                System.out.println(output);
-                output = br.readLine();
-            }
-            conn.disconnect();
-        }catch(Exception e){System.out.println(e);}
-    }
-    
-    public void post()
-    {
-        try
-        {
-            conn.connect();
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Accept", "application/json");
-            conn.setDoOutput(true);
-            input = "{ \"employeeId\": 10,\"firstName\": \"mario\",\"lastName\": \"rossi\", \"email\": \"mario@rossi.com\", \"phone\": \"3568945\" }";
-            os.write(input.getBytes());
-            os.flush();
-            conn.getResponseCode();
-            
+            br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            //leggo il body della risposta
             output = br.readLine();
             while(output!=null)
             {
@@ -66,5 +43,130 @@ public class RestClient
             }
             conn.disconnect();
         }catch(Exception e){}
+    }
+    
+    public void post(int id, String nome, String cognome, String email, String telefono)
+    {
+        try
+        {
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
+            conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            //invio il json della richiesta
+            input = "{ \"employeeId\": "+id+",\"firstName\": \""+nome+"\",\"lastName\": \""+cognome+"\", \"email\": \""+email+"\", \"phone\": \""+telefono+"\" }";
+            os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+            //controllo il codice della risposta
+            if(conn.getResponseCode()==403)
+                System.out.println("ID già presente.");
+            else if(conn.getResponseCode()!=201)
+                System.out.println("Inserimento non riuscito.");
+            else
+                System.out.println("Inserimento riuscito.");
+            conn.disconnect();
+        }catch(Exception e)
+        {}
+    }
+    
+    public void delete(int id)
+    {
+        try
+        {
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+            conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("DELETE");
+            conn.setRequestProperty("Accept", "application/json");
+            //controllo il codice di risposta
+            if(conn.getResponseCode()==204)
+                System.out.println("ID non trovato.");
+            else if(conn.getResponseCode()!=200)
+                System.out.println("Cancellazione non riuscita.");
+            else
+                System.out.println("Cancellazione riuscita.");
+            conn.disconnect();
+        }catch(Exception e){}
+    }
+    
+    public void getId(int id)
+    {
+        try
+        {
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+            conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            //controllo codice di risposta
+            if(conn.getResponseCode()==500)
+                System.out.println("ID non trovato.");
+            //leggo il body della risposta
+            br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            output = br.readLine();
+            while(output!=null)
+            {
+                System.out.println(output);
+                output=br.readLine();
+            }
+            conn.disconnect();
+        }catch(Exception e){}
+    }
+    //non implementato
+    public void patch(int id)
+    {
+        try
+        {
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+            conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("PATCH");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            //json della richiesta
+            input = "{ \"employeeId\": "+id+",\"firstName\": \"sara\",\"lastName\": \"bianchi\", \"email\": \"sara@bianchi.com\", \"phone\": \"3568945\" }";
+            os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+            conn.disconnect();
+        }catch(Exception e){}   
+    }
+    
+    public void put(int id, String nome, String cognome, String email, String telefono)
+    {
+        try
+        { 
+            //apertura connessione
+            url = new URL("http://localhost:8080/api/tutorial/1.0/employees/"+id);
+            conn = (HttpURLConnection) url.openConnection();
+            
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+            //input della richiesta
+            input = "{ \"employeeId\": "+id+",\"firstName\": \""+nome+"\",\"lastName\": \""+cognome+"\", \"email\": \""+email+"\", \"phone\": \""+telefono+"\" }";
+            os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+            //controllo codice di risposta
+            if(conn.getResponseCode()==204)
+                System.out.println("ID non trovato.");
+            else if(conn.getResponseCode()!=200)
+                System.out.println("Aggiornamento non riuscito.");
+            else
+                System.out.println("Aggiornamento riuscito.");
+
+            conn.disconnect();
+        }catch(Exception e){}   
     }
 }
